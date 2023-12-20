e3deb0a045654e927f67090630f3eb29ff3e4d4d
Merryjeena <merryjeena_n22@mtu.ac.in>
2023-12-20 15:01:05 +0530

    Calculator in C

---
+++ cal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+	 int sum=0,diff=0,a,b,c,d;
+	printf("Enter the nos to be added: ");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("Sum= ",sum);
+	 printf("Enter the nos to be substracted: ");
+        scanf("%d%d",&c,&d);
+        diff=c-d;
+        printf("Difference= ",diff);
+	return 0
+}
