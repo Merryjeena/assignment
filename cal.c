1e033e1896ea45f844afd261d68558bce91107e0
Merryjeena <150140290+Merryjeena@users.noreply.github.com>
2024-10-02 10:09:39 +0530

    Update cal.cc

--- cal.c
+++ cal.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
-main()
-{
-	 int sum=0,diff=0,a,b,c,d;
-	printf("Enter the nos to be added: ");
-	scanf("%d%d",&a,&b);
-	sum=a+b;
-	printf("Sum= ",sum);
-	 printf("Enter the nos to be substracted: ");
-        scanf("%d%d",&c,&d);
-        diff=c-d;
-        printf("Difference= ",diff);
-	return 0
+
+int main() {
+    char operator;
+    double num1, num2;
+    
+    // Prompt the user to enter an operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    
+    // Prompt the user to enter two numbers
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+    
+    // Perform the operation based on the entered operator
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            break;
+    }
+
+    return 0;
 }
-edited
